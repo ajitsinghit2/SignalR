54d223585f2904d9a6460567218da1079d8ea45c
NTaylorMullen <NTaylorMullen@outlook.com>
2013-06-20 14:18:59 -0700

    Added tracking for the negotiate request so calling stop properly aborts the request.
    - Without aborting the negotiate request starting and stopping in quick succession can cause multiple active ajax requests attempting to connect.
    
    #2601

--- src/Microsoft.AspNet.SignalR.Client.JS/jquery.signalR.core.js
+++ src/Microsoft.AspNet.SignalR.Client.JS/jquery.signalR.core.js
@@ -235,7 +235,9 @@
         init: function (url, qs, logging) {
             this.url = url;
             this.qs = qs;
-            this._ = {};
+            this._ = {
+                negotiateAbortText: "__Negotiate Aborted__"
+            };
             if (typeof (logging) === "boolean") {
                 this.logging = logging;
             }
@@ -479,7 +481,9 @@
             url = signalR.transports._logic.addQs(url, connection);
 
             connection.log("Negotiating with '" + url + "'.");
-            $.ajax(
+
+            // Save the ajax negotiate request object so we can abort it if stop is called while the request is in flight.
+            connection._.negotiateRequest = $.ajax(
                 $.extend({}, $.signalR.ajaxDefaults, {
                     xhrFields: { withCredentials: connection.withCredentials },
                     url: url,
@@ -487,11 +491,17 @@
                     contentType: connection.contentType,
                     data: {},
                     dataType: connection.ajaxDataType,
-                    error: function (error) {
-                        $(connection).triggerHandler(events.onError, [error.responseText]);
-                        deferred.reject("SignalR: Error during negotiation request: " + error.responseText);
-                        // Stop the connection if negotiate failed
-                        connection.stop();
+                    error: function (error, statusText) {
+                        // We don't want to cause any errors if we're aborting our own negotiate request.
+                        if (statusText !== connection._.negotiateAbortText) {
+                            $(connection).triggerHandler(events.onError, [error.responseText]);
+                            deferred.reject("SignalR: Error during negotiation request: " + error.responseText);
+                            // Stop the connection if negotiate failed
+                            connection.stop();
+                        } else {
+                            // This rejection will noop if the deferred has already been resolved or rejected.
+                            deferred.reject("Stopped the connection while negotiating.");
+                        }
                     },
                     success: function (res) {
                         var keepAliveData = connection.keepAliveData;
@@ -728,6 +738,12 @@
 
                     connection.transport.stop(connection);
                     connection.transport = null;
+                }
+
+                if (connection._.negotiateRequest) {
+                    // If the negotiation request has already completed this will noop.
+                    connection._.negotiateRequest.abort(connection._.negotiateAbortText);
+                    delete connection._.negotiateRequest;
                 }
 
                 // Trigger the disconnect event
